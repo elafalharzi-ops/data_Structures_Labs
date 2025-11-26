944dfed94312d1cca4ee1473464c8e94f765089e
ADMIN <elafalharzi@gmail.com>
2025-11-25 19:01:31 -0800

    Arrays

--- src/Array/array.java
+++ src/Array/array.java
@@ -1,4 +1,30 @@
 package Array;
-
+import java.util.Scanner;
 public class array {
+    public void Traversal(int []a){
+        for( int i=0;i<a.length;i++)
+            System.out.println("index["+i+"]="+a[i]);
+
+    }
+    public void Traversal2(int []a){
+        for( int e:a)
+            System.out.println(e);
+    }
+    public void R_Traversal(int []a){
+        for(int i=a.length-1;i>=0;i--)
+            System.out.println("index["+i+"]="+a[i]);
+    }
+    public void input(int[]a){
+        System.out.println("enter"+a.length+"numbers:");
+        Scanner scanner=new Scanner(System.in);
+        for(int i=0;i<a.length;i++)
+            a[i]=scanner.nextInt();
+    }
+    public void update(int []a,int index,int nvalue){
+        a[index]=nvalue;
+    }
+    public void delete(int []a, int index){
+        a[index]=0;
+    }
+
 }
--- src/Array/main.java
+++ src/Array/main.java
@@ -1,7 +1,19 @@
 package Array;
 
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
-        System.out.print("hi");
-    }
-}
+       array  arr=new array();
+    int numbers[]={1,2,3,4,5};
+//        arr.Traversal(numbers);
+//        arr.Traversal2(numbers);
+//        arr.R_Traversal(numbers);
+//        arr.input(numbers);
+        arr.update(numbers,3,3);
+        System.out.println(Arrays.toString(numbers));
+        arr.delete(numbers,0);
+        System.out.println(Arrays.toString(numbers));
+
+
+}}
