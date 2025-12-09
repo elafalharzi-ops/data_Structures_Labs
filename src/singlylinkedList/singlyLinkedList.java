a0c8cabefd2258c28fc9a032be42cd583e1cd7db
ADMIN <elafalharzi@gmail.com>
2025-12-09 09:14:45 -0800

    Arrays

--- src/singlylinkedList/singlyLinkedList.java
+++ src/singlylinkedList/singlyLinkedList.java
@@ -16,7 +16,7 @@ public class singlyLinkedList <E>{
         Node<E> nnod= new Node<>(head,data);
         head=nnod;
         if(isEmpty())
-            head=tail;
+           tail=head;
         size++;
     }
     public E getFrist(){
@@ -27,12 +27,58 @@ public class singlyLinkedList <E>{
         E delet=head.getData();
         head=head.getNext();
         if(size()==0)
-            head=tail;
+            tail=head;
         return delet;
 
 
+    }public  void addlast(E data){
+        Node <E > nnode=new Node(null,data);
+        if (isEmpty()){
+            head=nnode;
+        }
+        else {
+            tail.setNext(nnode);
+        }tail=nnode;
+        size++;
     }
+    public E getlast(){
+        if (isEmpty())
+            return null;
+        else{
+        return tail.getData();}
+    }
+    public E removelast(){
+        E delet=tail.getData();
+        if(isEmpty()) return null;
+        if (head== tail){
+            head=tail=null;}
+        else{
 
+            Node<E> temp = head;
+            while (temp.getNext()!= tail){
+                temp.getNext();
+            }temp.setNext(null);
+            tail=temp;
+
+
+        }size--;
+
+       return delet;
+    }public void display(){
+        Node<E> temp=head;
+        while (temp!=null){
+            System.out.print(temp.getData()+"---->");
+            temp=temp.getNext();
+        }System.out.print("null");
+    }
+    public void addatposition(E data,int position){
+
+        if (position<=0 ||position>size+1)
+            throw new IllegalArgumentException("invaild position");
+        if (position==1){
+            addFrist(data);
+        return;}
+    }
 
 
 
--- src/singlylinkedList/singlyLinkedList_main.java
+++ src/singlylinkedList/singlyLinkedList_main.java
@@ -1,4 +1,17 @@
 package singlylinkedList;
 
 public class singlyLinkedList_main {
+    public static void main(String[] args) {
+        singlyLinkedList<Integer> list = new singlyLinkedList<Integer>();
+        list.addFrist(1);
+        list.addFrist(2);
+        list.addFrist(3);
+        list.addFrist(4);
+        list.addFrist(5);
+        Integer last=list.getlast();
+        System.out.println(last);
+        list.display();
+
+    }
+
 }
